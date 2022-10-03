36c963b95d929d9c777e8aca695f847279f6bdf4
Gordon Ball <gball261@gmail.com>
2022-10-03 19:46:04 -0400

    Finished part one

--- partOne.js
+++ partOne.js
@@ -1,12 +1,15 @@
 "use strict";
 
+// Number 1
 let $img = $("article img");
 $img.addClass("image-center");
+
+// Number 2
 let $p = $("article p");
 $p.last().remove();
-$('h1').css({'font-size': randNum0to100() + 'px'});
 
-
+// Number 3
+$('h1').css({ 'font-size': randNum0to100() + 'px' });
 
 /**
  * Returns a random number from 0 - 100
@@ -14,3 +17,35 @@ $('h1').css({'font-size': randNum0to100() + 'px'});
 function randNum0to100() {
   return Math.floor(Math.random() * 101);
 }
+
+// Number 4
+let $newLi = $("<li>New list item!</li>");
+$("ol").append($newLi);
+
+// Number 5
+$("aside").empty();
+$("aside").append("<p>Lists suck, sorry.</p>");
+
+// Number 6
+let $numberForms = $(".form-control");
+
+$(".row.mb-5").on("change", ".form-control", changeColor)
+
+/** Change background color of body based on rgb values in input forms. */
+function changeColor() {
+  let colorVals = [];
+  $numberForms.each(function () {
+    colorVals.push($(this).val());
+  });
+  $("body").css("background-color", `rgb(${colorVals[0]}, ${colorVals[1]}, ${colorVals[2]})`);
+}
+
+
+// Number 7 - removes image when clicked
+$img.on("click", function () {
+  $img.remove();
+})
+
+
+
+
